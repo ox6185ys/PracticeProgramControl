220068086807e0ff7c718997621007a2da296407
Patrick <ox6185ys@go.minneapolis.edu>
2016-03-01 12:04:08 -0600

    Java Practice Program Control

---
+++ src/com/patrick/Main.java
@@ -0,0 +1,37 @@
+package com.patrick;
+import java.util.*;
+public class Main {
+//Guess the letter game.
+
+    public static void main(String[] args) throws java.io.IOException{
+/*        char ch, answer = 'K';
+
+        System.out.println("I'm thinking of a letter between A and Z.");
+        System.out.println("Can you guess the letter?");
+
+        ch = (char) System.in.read();
+
+        if (ch == answer) System.out.println("**RIGHT**");
+        else {
+            System.out.println("...Sorry, you're wrong...");
+            if (ch < answer) System.out.println("too low.");
+            else System.out.println("too high.");
+        }*/
+/*        int x ;
+
+        for (x = 0; x < 6; x++){
+            if (x==1)
+                System.out.println("x is one");
+            else if (x==2)
+                System.out.println("x is two");
+            else if (x==3)
+                System.out.println("x is three");
+            else if (x==4)
+                System.out.println("x is four");
+            else
+                System.out.println("x is not between 1 and 4");
+        }*/
+
+
+    }
+}
